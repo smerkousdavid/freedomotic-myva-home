38df1609bb94cd89d0c9fd10cce8261a0f77ebce
David Smerkous <David Smerkous>
2016-05-06 22:24:53 -0500

    Base (MYVA)

---
+++ app/src/main/java/com/smerkous/david/homeautomation/RESTful.java
@@ -0,0 +1,13 @@
+package com.smerkous.david.homeautomation;
+
+/**
+ * Created by David on 5/6/2016.
+ * For the use of a static instance of RESTapi
+ */
+public class RESTful {
+
+    public static void RESTinit() {
+
+    }
+
+}
